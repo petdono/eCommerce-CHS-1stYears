6e4e367387df0a1a80649ea8a6ab7367d324a648
arcade-smasher <83091369+arcade-smasher@users.noreply.github.com>
2026-02-05 13:39:34 -0500

    add comments

--- main/__init__.py
+++ main/__init__.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
+// initialize main blueprint
 main = Blueprint('main', __name__, template_folder='templates')
 
+// main route
 @main.route('/', defaults={'page': 'index'})
 @main.route('/<page>')
 def show(page):
     try:
         return render_template(f'pages/{page}.html')
     except TemplateNotFound:
-        abort(404)
+        abort(404) // 404, handle later with 404 page
