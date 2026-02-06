328b50df825d804a2cc65c2eb8c3843ef7d2dcdf
arcade-smasher <83091369+arcade-smasher@users.noreply.github.com>
2026-02-06 13:25:35 -0500

    add necessary comments

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, render_template, abort
-from jinja2 import TemplateNotFound
+from flask import Blueprint, render_template, abort # import blueprint, render template, and abort from flask
+from jinja2 import TemplateNotFound # import TemplateNotFound error from jinja2
 
-auth = Blueprint('auth', __name__, template_folder='templates')
+auth = Blueprint('auth', __name__, template_folder='templates') # initialize the bluetooth with the auth template in the `templates` template folder
 
-@auth.route('/', defaults={'page': 'index'})
-@auth.route('/<page>')
-def show(page):
-    try:
-        return render_template(f'pages/{page}.html')
-    except TemplateNotFound:
-        abort(404)
+@auth.route('/', defaults={'page': 'index'}) # make a default route if no page is specified
+@auth.route('/<page>') # handle other routes
+def show(page): # show page function
+    try: # try
+        return render_template(f'pages/{page}.html') # return the template with the page name
+    except TemplateNotFound: # handle the TemplateNotFound exception
+        abort(404) # send 404 to the client if template not found
